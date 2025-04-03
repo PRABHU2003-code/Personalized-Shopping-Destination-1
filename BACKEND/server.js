91f919a2a9d1a1fc51f6922fe6a67602b6344dc8
PRABHU S <prabhus.23mca@kongu.edu>
2025-04-03 19:17:56 +0530

    Add files via upload

---
+++ BACKEND/server.js
@@ -0,0 +1,59 @@
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+import uploadRouter from "./routes/uploadRoutes.js";
+import { fileURLToPath } from 'url';
+//const stripe = require('stripe')('sk_test_51O6otaSFsGF8VKTobGgjrnHavxhJbPEtCBgwKGrInrhvbIHmVyYHIJfXZw1KVf61p6quGNIFRoCdwWInnoQlQ2gy000C4Xvyoz');
+
+dotenv.config();
+
+mongoose
+  .connect('mongodb://127.0.0.1:27017/test_db')
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const __filename = fileURLToPath(import.meta.url);
+//const __dirname = path.dirname(__filename);
+//app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.get("/api/keys/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
+app.get("/api/keys/google", (req, res) => {
+  res.send({ key: process.env.GOOGLE_API_KEY || "" });
+});
+
+app.use("/api/upload", uploadRouter);
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
